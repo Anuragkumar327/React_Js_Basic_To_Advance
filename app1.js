6919e4cd79f452447f40a39491032038cfb2e283
Anurag Kumar <januragkumar327@gmail.com>
2026-04-03 00:39:24 +0530

    Add updated files

---
+++ app1.js
@@ -0,0 +1,29 @@
+
+// const a = 10;
+
+// export default a 
+
+
+
+
+
+// const arr = [10,20,30,40,50]
+
+// const user = 'Anurag Jha'
+
+// export const Hello = "Helo this is Anurag jha"
+
+// export default arr  // this will decide what to print
+
+
+const User = 'Sarthak'
+export const age = 24
+export const skill = ['Frontend','Backend','FullStack']
+
+export default User
+
+
+
+
+//  export are of two type Named and  Default 
+
---
+++ script.js
@@ -0,0 +1,21 @@
+
+// import a from './app1.js'
+
+// import {Hello} from './app1.js' // This is called name Export
+
+// console.log(Hello);
+
+// console.log(a);
+
+import user from './app1.js'
+
+console.log(user)
+
+import { age } from './app1.js'
+
+import { skill } from './app1.js'
+
+console.log(age)
+
+console.log(skill);
+
